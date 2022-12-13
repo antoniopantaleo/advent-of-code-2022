a5479212d30096a4b3369ddd6576b0fdfe387215
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2022-12-14 00:03:07 +0100

    add Day 5 part 1

---
+++ 5/partOne.py
@@ -0,0 +1,38 @@
+import re
+
+DEMO = False
+
+data = open("input-demo.txt" if DEMO else "input.txt").read()
+data = data.split("\n\n")
+
+stacks_number = data[0].split("\n")[-1]
+stacks_number = len(re.findall("\d+", stacks_number))
+
+lines = []
+stacks = []
+
+for i in range(0,stacks_number):
+    line = data[0].split("\n")[i]
+    line = re.sub("\s{1,2}", "-", line)
+    line = re.sub("[\[\]]","",line)
+    lines.append(line)
+
+for i in range(0, stacks_number*2, 2):
+    stack = []
+    for line in lines:
+        if match := re.search("[A-Z]", line[i]):
+            stack.append(match.group())
+    stacks.append(stack)
+
+def make_move(instruction):
+    move = int(re.search("(?<=move )\d+", instruction).group())
+    _from = int(re.search("(?<=from )\d+", instruction).group()) - 1
+    to = int(re.search("(?<=to )\d+", instruction).group()) - 1
+    for i in range(0, move):
+        block = stacks[_from].pop(0)
+        stacks[to].insert(0, block)
+
+for idx, instruction in enumerate(data[1].split("\n")):
+    make_move(instruction)
+
+print("".join([stack[0] for stack in stacks]))
